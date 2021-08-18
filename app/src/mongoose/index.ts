1b0070cab291e72b1928d6daed5acf04ac72316c
lightcodes845 <dare.falola7@gmail.com>
2021-08-18 15:45:39 +0100

    updates

--- app/src/mongoose/index.ts
+++ app/src/mongoose/index.ts
@@ -1,8 +1,9 @@
 import * as mongoose from 'mongoose';
 import { config } from '../config/mongoose';
-
+import { User } from '../auth/models/user.model';
 const connectDB = async () => {
   try {
+    console.log('config ', config);
     await mongoose.connect(
       `mongodb://${config.user}:${config.password}@${config.podName}-0.${config.host}:27017,${config.podName}-1.${config.host}:27017,${config.podName}-2.${config.host}:27017/?authSource=admin&replicaSet=rs0`,
       {
@@ -14,6 +15,8 @@ const connectDB = async () => {
       },
     );
     console.log('Connected to Mongo DB');
+    const users = await User.find({});
+    console.log('users ', users);
   } catch (e) {
     console.log(e);
   }
